29b4b801f1f918fe560d7e52d281f9c3aecd308d
bseefeldt37 <benjamin.seefeldt@outlook.com>
2022-09-15 10:13:11 -0500

    Created Get-CSV function

--- ADBulkImport.ps1
+++ ADBulkImport.ps1
@@ -10,13 +10,30 @@ DESCRIPTION:
 #>
 
 ### VARIABLE DECLORATION ###
-$input = Get-Host "Please make a selection"
+$input = $null
 
 
+### FUNCTION CREATION ###
+function Get-CSV {
+    # Obtaining file path and name
+    Write-Host "Getting file path..." -ForegroundColor Cyan
+    $CSVpath = Read-Host "Enter the file path"
+    $CSVname = Read-Host "Enter the filename of the Bulk Users .csv"
+    $CSVfull = "$CSVpath\$CSVname"
+    
+    if ((Test-Path -Path "$CSVfull") -eq $false ) {
+        <# Action to perform if the condition is true #>
+        Write-Host "The file $CSVname cannot be found in $CSVpath"
+    }
+    else {
+        Write-Host "File has been found"
+    }
+}
 
 ### BODY ###
 try {
-    
+    Clear-Host
+
 }
 catch {
     <#Do this if a terminating exception happens#>
