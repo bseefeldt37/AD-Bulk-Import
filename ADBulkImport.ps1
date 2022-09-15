c166cee38dc124aabedc5f287eab249f71d6cb0a
bseefeldt37 <benjamin.seefeldt@outlook.com>
2022-09-15 09:00:58 -0500

    Initialization

---
+++ ADBulkImport.ps1
@@ -0,0 +1,26 @@
+<#
+SCRIPT BY:  Benjamin Seefeldt
+CREATED:    15 Sep 22
+UPDATED:    15 Sep 22
+
+DESCRIPTION:
+    This script is to perform the bulk import of AD users from a .csv file.
+
+
+#>
+
+### VARIABLE DECLORATION ###
+$input = Get-Host "Please make a selection"
+
+
+
+### BODY ###
+try {
+    
+}
+catch {
+    <#Do this if a terminating exception happens#>
+}
+finally {
+    <#Do this after the try block regardless of whether an exception occurred or not#>
+}
